46e54c51679ae86702c73dfe91bf470631105b8a
carkings <qingzi.wang@cq-tct.com>
2017-03-28 14:08:08 +0800

    little modify

--- src/routes/IndexPage.jsx
+++ src/routes/IndexPage.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
 
-function IndexPage() {
+const IndexPage = () => {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!!!</h1>
+      <h1 className={styles.title}>Yay! Welcome to carkings dva!!!</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
